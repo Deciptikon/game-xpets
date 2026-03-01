46c74603116a5bac9ea1fccd7bddf97fcaaff9ef
Deciptikon <deciptikon@mail.ru>
2026-03-01 19:57:06 +0300

    ////

--- app/scenes/PetsScene.js
+++ app/scenes/PetsScene.js
@@ -7,30 +7,40 @@ import { ListPets } from "../pets/listPets.js";
 export default class PetsScene extends Phaser.Scene {
   constructor() {
     super({ key: "PetsScene" });
+
+    this.buttons = {};
   }
 
   create() {
     //this.gameState = this.game.registry.get("gameState");
     //this.gameState.loadSettings();
 
-    let btt = new IconButton(
-      this,
-      W / 2,
-      H / 2,
-      100,
-      100,
-      "icon_cat",
-      null,
-      () => {
-        console.log("!!!!!!!!");
-        btt.x = btt.x < W / 2 ? W * 0.75 : W * 0.25;
-      },
-      { scale: 0.4, hoverScale: 0.5 },
-    );
+    this.createAllPets();
 
     console.log("PetsScene");
     newBackButton(this);
 
     console.log(ListPets);
+  }
+
+  createAllPets() {
+    let x = 100;
+    for (const key in ListPets) {
+      const btt = new IconButton(
+        this,
+        x,
+        H / 2,
+        100,
+        100,
+        `icon_${key}`,
+        null,
+        () => {
+          console.log(`icon_${key}`);
+        },
+        { scale: 0.4, hoverScale: 0.5 },
+      );
+      this.buttons[key] = btt;
+      x += 300;
+    }
   }
 }
